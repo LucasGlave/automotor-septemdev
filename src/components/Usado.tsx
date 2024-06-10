7d77673721e40d3e20830342245e7d4958f8f3d3
Franco Prandi <francoprandi55@gmail.com>
2024-06-10 18:01:41 -0300

    feat: usado & usados updates

--- src/components/Usado.tsx
+++ src/components/Usado.tsx
@@ -6,12 +6,12 @@ type usadoProps = {
 
 const Usado = ({ img, model, price }: usadoProps) => {
   return (
-    <div className="flex w-full h-full">
+    <div className="flex justify-center h-full">
       <div className="flex items-center flex-col justify-center m-auto">
         <img
           src={img}
           alt="nosotros"
-          className="z-10 rounded-xl shadow-2xl w-[95%] h-[13rem]"
+          className="z-10 rounded-xl shadow-2xl w-[20rem] h-[13rem] object-cover"
         />
         <div className="bg-[#ffffffcd] rounded-b-lg p-4 xl:w-[85%] w-[85%] flex justify-between text-black">
           <div>
