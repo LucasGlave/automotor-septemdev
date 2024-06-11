ae12b611128501ed6d6d986f21d605b0de830aea
Franco Prandi <francoprandi55@gmail.com>
2024-06-11 18:44:32 -0300

    feat: folders updates

--- src/components/Vehiculos/Usado.tsx
+++ src/components/Vehiculos/Usado.tsx
@@ -1,22 +1,26 @@
 type usadoProps = {
-  img: string;
+  img?: string;
+  brand: string;
   model: string;
   price: string;
 };
 
-const Usado = ({ img, model, price }: usadoProps) => {
+const Usado = ({ img, model, price, brand }: usadoProps) => {
   return (
     <div className="flex justify-center h-full">
       <div className="flex items-center flex-col justify-center m-auto">
-        <img
-          src={img}
-          alt="nosotros"
-          className="z-10 rounded-xl shadow-2xl w-[20rem] h-[13rem] object-cover"
-        />
+        <a>
+          <img
+            src={img}
+            alt="nosotros"
+            className="z-10 rounded-xl shadow-2xl w-[20rem] h-[13rem] object-cover hover:scale-105 transform duration-300 cursor-pointer"
+          />
+        </a>
+
         <div className="bg-[#ffffffcd] rounded-b-lg p-4 xl:w-[85%] w-[85%] flex justify-between text-black">
           <div>
             <p>
-              <b>{model}</b>
+              <b>{brand} {model}</b>
             </p>
             <p>${price}</p>
           </div>
