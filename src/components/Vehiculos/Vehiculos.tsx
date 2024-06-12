41dc6f01b467b04d7835f5520db50ad814923170
Franco Prandi <francoprandi55@gmail.com>
2024-06-12 12:14:44 -0300

    vehiculos & usados updates

--- src/components/Vehiculos/Vehiculos.tsx
+++ src/components/Vehiculos/Vehiculos.tsx
@@ -1,25 +1,29 @@
-import { vehiculos } from './dataVehiculos'
-import Usado from './Usado'
+import { vehiculos } from "./dataVehiculos";
+import Usado from "./Usado";
 
 function Vehiculos() {
   return (
-    <div>
-      <div className="w-full min-h-screen flex flex-col text-start justify-center z-10">
-      <div className="flex flex-col items-center justify-center gap-10 w-full  h-[90%] bg-gradient-to-b from-transparent via-[rgba(255,255,255,0.68)] via-50% to-transparent p-[5%] z-10">
+    <div className="w-full min-h-screen flex flex-col z-10">
+      <div className="flex flex-col xl:mt-8 md:mt-12 mt-16  items-center justify-center gap-10 w-full h-[90%] bg-gradient-to-b from-transparent via-[rgba(255,255,255,0.68)] via-50% to-transparent p-[5%] z-10">
         <h2 className="text-white font-extrabold z-10 text-3xl font-roboto text-shadow-md">
           VEHÍCULOS
         </h2>
-        <div className=" m-auto flex items-center justify-center">
-          <div className="w-[75%] flex flex-wrap gap-8 m-auto justify-center">
-            {vehiculos.map((vehiculo, index) => (
-              <Usado key={index} img={vehiculo.img} brand={vehiculo.marca} model={vehiculo.modelo} price={vehiculo.precio} />
-            ))}
-          </div>
+      </div>
+      <div className="flex justify-center items-center w-full">
+        <div className="w-[80%] justify-center flex flex-wrap gap-8 text-start">
+          {vehiculos.map((vehiculo, index) => (
+            <Usado
+              key={index}
+              img={vehiculo.img}
+              brand={vehiculo.marca}
+              model={vehiculo.modelo}
+              price={vehiculo.precio}
+            />
+          ))}
         </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Vehiculos
+export default Vehiculos;
